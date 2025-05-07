0f209eb545b819f30988f473c641d4f6c7d076bd
unknown <josuenoj@kinal.edu.gt>
2025-05-07 17:25:37 -0600

    Feat: implementación de rutas principales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useRoutes } from "react-router-dom"
+import { routes } from "./routes"
+
 function App() {
+  const mainRoutes = useRoutes(routes)
 
   return (
     <>
-     
+      { mainRoutes }
     </>
   )
 }
